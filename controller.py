1c477b08aa23221895bebdbeb4240eaa208e67e3
Aimee Morgan <alm@poshlsot.(none)>
2013-09-02 11:24:22 -0700

    initial commit

---
+++ controller.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
+
